754c2435b89df4cfdd8561b8923c8d477b84b006
Marckos Agency (Shahanwaz) <111874347+shahanwaztech@users.noreply.github.com>
2024-05-24 21:47:20 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
-import BottomText from "@/components/bottom-text";
-import NavBar from "@/components/navbar";
 
 const inter = Inter({subsets: ["latin"]});
 
